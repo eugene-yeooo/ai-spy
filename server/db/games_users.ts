9bde74f4443783bcc745876afd7de7ead1ce2d22
Oranee-Kasem-Whai-25 <ornks43@gmail.com>
2025-06-12 21:36:49 +1200

    function getAllLogsWithUsername

--- server/db/games_users.ts
+++ server/db/games_users.ts
@@ -3,9 +3,18 @@ import db from './connection.ts'
 
 export async function getAllLogsWithUsername() {
 
-const logs = await db('game_logs')
-.join('users', 'game_logs.user_id', 'users.id')
-.select('game_logs', 'users.username')
-.orderBy('game_logs.created_at')
+const logs = await db('gameslog')
+.join('users', 'gameslog.user_id', 'users.id')
+.select('gameslog.id as gameslog_id',
+   'user.id as user_id', 
+   'gameslog.status',
+   'gameslog.question_used',
+   'gameslog.hint_used',
+   'gameslog.ai_answer',
+   'gameslog.question_text',
+   'gameslog.ai_response',
+   'gameslog.created_at'
+  )
+.orderBy('gameslog.created_at')
 return logs
 }
