5d3ed1c76227e78471f8832836a93c6d3a9d0ae5
Hannah Smith <hchristinasmith@gmail.com>
2025-06-13 11:35:57 +1200

    draft

--- client/pages/Game.tsx
+++ client/pages/Game.tsx
@@ -1,8 +1,10 @@
+import GameLogic from '../components/Game'
+
 function Game() {
   return (
     <>
       <div className="app">
-        <h1>Game</h1>
+        <GameLogic />
       </div>
     </>
   )
