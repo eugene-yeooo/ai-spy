510ea621485a3f10c561fcb7bb2e40c2b67633df
Kevin Lai <kevinllai@hotmail.com>
2025-06-12 17:37:16 +1200

    added initial pages and routes for frontend

---
+++ client/pages/Game.tsx
@@ -0,0 +1,11 @@
+function Game() {
+  return (
+    <>
+      <div className="app">
+        <h1>Game</h1>
+      </div>
+    </>
+  )
+}
+
+export default Game
---
+++ client/pages/GameLog.tsx
@@ -0,0 +1,11 @@
+function GameLog() {
+  return (
+    <>
+      <div className="app">
+        <h1>Game Log</h1>
+      </div>
+    </>
+  )
+}
+
+export default GameLog
