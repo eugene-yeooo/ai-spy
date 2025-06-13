398066620db70c25f88c2b2e42fba0cb60d0569e
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-06-13 14:30:32 +1200

    move login div to Login.tsx from Layout.tsx

--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -1,48 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-import { IfAuthenticated, IfNotAuthenticated } from '../utilities/Authenticated'
 
 export default function Layout() {
-  const { user, logout, loginWithRedirect } = useAuth0()
-
-  const handleSignOut = () => {
-    return logout()
-  }
-
-  const handleSignIn = () => {
-    return loginWithRedirect()
-  }
-
-  const buttonStyle = 'fixed right-4 top-4 border px-2 bg-gray-200 rounded'
-
   return (
     <>
       <header>
         <h1>AI SPY</h1>
-
-        <div>
-          <IfAuthenticated>
-            <div className="fixed right-4 top-2">
-              <button onClick={handleSignOut} className={buttonStyle}>
-                Sign out
-              </button>
-              {user && (
-                <img
-                  src={user?.picture}
-                  alt={user?.given_name}
-                  referrerPolicy="no-referrer"
-                  className="h-30 w-30 rounded-full"
-                />
-              )}
-              {user && <p>Signed in as: {user?.nickname}</p>}
-            </div>
-          </IfAuthenticated>
-          <IfNotAuthenticated>
-            <button onClick={handleSignIn} className={buttonStyle}>
-              Sign in
-            </button>
-          </IfNotAuthenticated>
-        </div>
       </header>
       <main>
         <Outlet />
--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,10 +1,48 @@
 import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from '../utilities/Authenticated'
 
 function Login() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleSignOut = () => {
+    return logout()
+  }
+
+  const handleSignIn = () => {
+    return loginWithRedirect()
+  }
+
+  const buttonStyle =
+    'fixed right-4 top-4 border px-2 py-1 bg-black text-white rounded'
+
   return (
     <>
       <div className="app">
         <h1>Login Page</h1>
+      </div>
+      <div>
+        <IfAuthenticated>
+          <div className="fixed right-4 top-2">
+            <button onClick={handleSignOut} className={buttonStyle}>
+              Sign out
+            </button>
+            {user && (
+              <img
+                src={user?.picture}
+                alt={user?.given_name}
+                referrerPolicy="no-referrer"
+                className="h-30 w-30 rounded-full"
+              />
+            )}
+            {user && <p>Signed in as: {user?.nickname}</p>}
+          </div>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <button onClick={handleSignIn} className={buttonStyle}>
+            Sign in
+          </button>
+        </IfNotAuthenticated>
       </div>
       <ul>
         <Link to="/game">
